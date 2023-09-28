5921ee88fe6103c417af93cbef505c66aa41b1dc
Abdulhakim50 <abduhakimzeyne@gmail.com>
2023-09-28 13:49:22 +0300

    ...

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   images:{
     domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
+  },
+   async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow all origins
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,PATCH,OPTIONS", // Allow all methods
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type,Authorization", // Allow all headers
+          },
+        ],
+      },
+    ];
   }
 }
 
