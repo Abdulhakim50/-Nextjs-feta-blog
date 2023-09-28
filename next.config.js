aa8054a6359f49ab877ea5d3bbc99986e1e7efbc
Abdulhakim50 <abduhakimzeyne@gmail.com>
2023-09-28 15:32:55 +0300

    config commit

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
    async headers() {
     return [
       {
-        source: "/https://feta-blogg.vercel.app/api/:path*",
+        source: "/api/:path*",
         headers: [
           {
             key: "Access-Control-Allow-Origin",
