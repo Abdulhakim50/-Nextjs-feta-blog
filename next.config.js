be33923d42bfd10b5dd1397f09d288da679ac912
Abdulhakim50 <abduhakimzeyne@gmail.com>
2023-09-29 22:54:57 +0300

    first commit

--- next.config.js
+++ next.config.js
@@ -3,7 +3,27 @@ const nextConfig = {
   images:{
     domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
   },
- 
+   async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Allow all origins
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,PATCH,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type,Authorization",
+          },
+        ],
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
