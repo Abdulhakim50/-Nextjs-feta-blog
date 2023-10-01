86ab7f8defacdf850c54b53ab7c17d5f4870b213
abd251 <81411376+abd251@users.noreply.github.com>
2023-10-01 14:07:54 +0300

    ...

--- src/components/motion/Animat.jsx
+++ src/components/motion/Animat.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useEffect,useRef } from "react";
-import { motion,useInView,useAnimation } from "node_modules/framer-motion/dist/framer-motion";
+import { motion,useInView,useAnimation } from "framer-motion";
 
 
 
