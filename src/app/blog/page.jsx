4b15fd166ead483a3eca19eb48da9d3ed1ededd4
abd251 <81411376+abd251@users.noreply.github.com>
2023-10-03 16:10:12 +0300

    ...

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -8,7 +8,7 @@ const BlogPage = ({ searchParams }) => {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>{cat} Blog</h1>
+      <h1 className={styles.title}>{cat}</h1>
       <div className={styles.content}>
         <CardList page={page} cat={cat}/>
         <Menu />
